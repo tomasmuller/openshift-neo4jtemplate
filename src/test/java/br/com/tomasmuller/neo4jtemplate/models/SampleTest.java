615cb424fdfcdcd7a07f08c45adafc98233f70f6
Tomás Augusto Müller <tomas.am@gmail.com>
2012-03-29 20:10:56 -0300

    Organized imports.

--- src/test/java/br/com/tomasmuller/neo4jtemplate/models/SampleTest.java
+++ src/test/java/br/com/tomasmuller/neo4jtemplate/models/SampleTest.java
@@ -1,18 +1,15 @@
 package br.com.tomasmuller.neo4jtemplate.models;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.template.Neo4jOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Iterator;
-
-import static org.junit.Assert.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
